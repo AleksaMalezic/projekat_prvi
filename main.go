577962296abcfa0fc6d0c50775cf8add03dae5ef
Aleksa Malezić <aleksa.malezic@gmail.com>
2024-06-06 12:20:58 +0200

    uradjenje funkcije za menjanje i brisanje

--- main.go
+++ main.go
@@ -37,9 +37,9 @@ func main() {
 	r.GET(page, getUsers)
 	r.GET(pageid, getUserByID)
 	r.POST(page, postUsers)
-	//r.PUT(page, putUsers)
-	//r.PATCH(page, patchUser)
-	//r.DELETE(page, deleteUsers)
+	r.PUT(pageid, putUser)
+	r.PATCH(pageid, patchUser)
+	r.DELETE(pageid, deleteUser)
 
 	r.Run(host)
 
@@ -71,4 +71,63 @@ func postUsers(c *gin.Context) {
 
 	users = append(users, newUser)
 	c.IndentedJSON(http.StatusCreated, newUser)
+}
+
+func putUser(c *gin.Context) {
+	id := c.Param("id")
+	var updatedUser user
+
+	if err := c.BindJSON(&updatedUser); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "invalid JSON"})
+		return
+	}
+
+	for i, a := range users {
+		if a.ID == id {
+			users[i] = updatedUser
+			c.IndentedJSON(http.StatusOK, updatedUser)
+			return
+		}
+	}
+	c.IndentedJSON(http.StatusNotFound, gin.H{"message": "user not found"})
+}
+
+func patchUser(c *gin.Context) {
+	id := c.Param("id")
+	var updatedFields map[string]interface{}
+
+	if err := c.BindJSON(&updatedFields); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "invalid JSON"})
+		return
+	}
+
+	for i, a := range users {
+		if a.ID == id {
+			if name, ok := updatedFields["name"].(string); ok {
+				users[i].Name = name
+			}
+			if age, ok := updatedFields["age"].(int); ok {
+				users[i].Age = age
+			}
+			if occupation, ok := updatedFields["occupation"].(string); ok {
+				users[i].Occupation = occupation
+			}
+			c.IndentedJSON(http.StatusOK, users[i])
+			return
+		}
+	}
+	c.IndentedJSON(http.StatusNotFound, gin.H{"message": "user not found"})
+}
+
+func deleteUser(c *gin.Context) {
+	id := c.Param("id")
+
+	for i, a := range users {
+		if a.ID == id {
+			users = append(users[:i], users[i+1:]...)
+			c.IndentedJSON(http.StatusOK, gin.H{"message": "user deleted"})
+			return
+		}
+	}
+	c.IndentedJSON(http.StatusNotFound, gin.H{"message": "user not found"})
 }
