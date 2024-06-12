52a4b25ca4ce28671fc64d58e969707f902acb0f
Aleksa Malezić <aleksa.malezic@gmail.com>
2024-06-12 11:54:12 +0200

    dodata je autentifikacija pomocu jwt tokena

--- main.go
+++ main.go
@@ -3,9 +3,10 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"time"
 
-	//"github.com/golang-jwt/jwt"
 	"github.com/gin-gonic/gin"
+	"github.com/golang-jwt/jwt"
 )
 
 // ovo cu posle da zamenim json fajlom
@@ -16,20 +17,23 @@ type user struct {
 	Occupation string `json:"occupation"`
 }
 
-var (
-	users    []user
-	jsonFile = "user.json"
-	page     = "/users"
-	pageid   = "/users/:id"
-	host     = "localhost:8080"
+type User struct {
+	Username string
+	Password string
+}
 
-	secretKey = []byte("secret-key")
+var (
+	users         []user
+	jsonFile      = "user.json"
+	page          = "/users"
+	pageid        = "/users/:id"
+	host          = "localhost:8080"
+	dummyUsername = "Aleksa"
+	dummyPassword = "ryko123"
+	secretKey     = []byte("secret-key")
 )
 
 func main() {
-
-	//token
-
 	//otvaranje json fajla
 	binaryFile := jsonToBinary(jsonFile)
 	defer binaryFile.Close()
@@ -38,19 +42,58 @@ func main() {
 	binToStruct(binaryFile, &users)
 
 	//HTTP requests
-	r := gin.Default()
+	router := gin.Default()
 
-	r.GET(page, getUsers)
-	r.GET(pageid, getUserByID)
-	r.POST(page, postUsers)
-	r.PUT(pageid, putUser)
-	r.PATCH(pageid, patchUser)
-	r.DELETE(pageid, deleteUser)
+	router.Use(authMiddleware())
 
-	r.Run(host)
+	router.POST("/login", loginHandler)
 
+	//zasticeni endpointi
+	protected := router.Group("/")
+	protected.Use(authMiddleware())
+	{
+		router.GET(page, getUsers)
+		router.GET(pageid, getUserByID)
+		router.POST(page, postUsers)
+		router.PUT(pageid, putUser)
+		router.PATCH(pageid, patchUser)
+		router.DELETE(pageid, deleteUser)
+		router.GET("/protected", ProtectedHandler)
+	}
+
+	router.Run(host)
 }
 
+func authMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		switch c.Request.URL.Path {
+		case "/login":
+			c.Next()
+			return
+		case "/users":
+			c.Next()
+			return
+		}
+
+		tokenString := c.GetHeader("Authorization")
+		if tokenString == "" {
+			c.JSON(http.StatusUnauthorized, gin.H{"error": "Missing authorization header"})
+			c.Abort()
+			return
+		}
+		tokenString = tokenString[len("Bearer "):]
+
+		err := verifyToken(tokenString)
+		if err != nil {
+			c.JSON(http.StatusUnauthorized, gin.H{"error": "Invalid token"})
+			c.Abort()
+			return
+		}
+		c.Next()
+	}
+}
+
+// funkcije za http zahteve
 func getUsers(c *gin.Context) {
 	c.IndentedJSON(http.StatusOK, users)
 }
@@ -107,7 +150,7 @@ func patchUser(c *gin.Context) {
 		return
 	}
 
-	fmt.Printf("Updated fields: %+v\n", updatedFields)
+	//fmt.Printf("Updated fields: %+v\n", updatedFields)
 
 	for i, a := range users {
 		if a.ID == id {
@@ -140,4 +183,58 @@ func deleteUser(c *gin.Context) {
 		}
 	}
 	c.IndentedJSON(http.StatusNotFound, gin.H{"message": "user not found"})
+}
+
+// funkcije za token
+func createToken(username string) (string, error) {
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
+		"username": username,
+		"exp":      time.Now().Add(time.Hour * 24).Unix(),
+	})
+
+	tokenString, err := token.SignedString(secretKey)
+	if err != nil {
+		return "", err
+	}
+
+	return tokenString, nil
+}
+
+func verifyToken(tokenString string) error {
+	token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+		return secretKey, nil
+	})
+
+	if err != nil {
+		return err
+	}
+
+	if !token.Valid {
+		return fmt.Errorf("invalid token")
+	}
+
+	return nil
+}
+
+func loginHandler(c *gin.Context) {
+	var u User
+	if err := c.BindJSON(&u); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "invalid JSON"})
+		return
+	}
+
+	if u.Username == dummyUsername && u.Password == dummyPassword {
+		tokenString, err := createToken(u.Username)
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "could not create token"})
+			return
+		}
+		c.JSON(http.StatusOK, gin.H{"token": tokenString})
+	} else {
+		c.JSON(http.StatusUnauthorized, gin.H{"error": "invalid credentials"})
+	}
+}
+
+func ProtectedHandler(c *gin.Context) {
+	c.JSON(http.StatusOK, gin.H{"Message": "Welcome to the protected area"})
 }
